ecacd4fb178c078474dd0d569a7d0c7afcda0886
hoovett <hoovett@gmail.com>
2024-02-09 18:49:07 +0100

    user entity(didn't save)

---
+++ src/Auction.API/Domain/Entities/User.cs
@@ -0,0 +1,31 @@
+using Auction.API.Common.Constants;
+using Auction.API.Domain.Enums;
+using Newtonsoft.Json;
+
+namespace Auction.API.Domain.Entities;
+
+public class User
+{
+    [JsonProperty("UserId")]
+    public string Id { get; set; }
+
+    [JsonRequired]
+    [JsonProperty("username")]
+    public string UserName { get; set; }
+
+    [JsonProperty("firstname")]
+    public string FirstName{ get; set; }
+
+    [JsonProperty("lastname")]
+    public string LastName{ get; set; }
+
+    [JsonProperty("email")]
+    public string Email { get; set; }
+
+    [JsonRequired]
+    [JsonProperty("passwordhash")]
+    public string PasswordHash { get; set; }
+
+    [JsonProperty("phonenumber")]
+    public string PhoneNumber { get; set; }
+}
