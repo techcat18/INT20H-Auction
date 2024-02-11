6675b15360a050751bccb4bd22210b2ff853e62a
demakk <dimademch03@gmail.com>
2024-02-11 14:51:23 +0200

    feat: chat-related features

---
+++ src/Auction.API/Domain/Entities/Chat.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Auction.API.Domain.Entities;
+
+public class Chat : BaseEntity
+{
+    [JsonProperty("lotId")]
+    [JsonRequired]
+    public string LotId { get; set; }
+
+    [JsonProperty("name")]
+    public string Name { get; set; }
+}
